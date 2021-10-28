8c6043fb16aa91960896d6ace3183aa4f74f1896
Leonardo Rocca <lrocca@student.42roma.it>
2021-10-28 19:41:58 +0200

    finish ex00?

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 20:52:06 by lrocca            #+#    #+#             */
-/*   Updated: 2021/10/27 21:00:55 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/10/28 19:41:26 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ int	main(int ac, char **av)
 {
 	(void)(av);
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
-		;
+		for (int i = 1; av[i]; i++)
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char)toupper(av[i][j]);
+	std::cout << std::endl;
 }
