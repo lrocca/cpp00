5f5eea83835be43be75afdc9938c5fabec532862
leeoocca <leeoocca@gmail.com>
2021-10-29 19:25:25 +0200

    fix megaphone not printing spaces

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 20:52:06 by lrocca            #+#    #+#             */
-/*   Updated: 2021/10/28 19:41:26 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/10/29 19:23:31 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,11 @@ int	main(int ac, char **av)
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
-		for (int i = 1; av[i]; i++)
+		for (int i = 1; i < ac; i++) {
 			for (int j = 0; av[i][j]; j++)
 				std::cout << (char)toupper(av[i][j]);
+			if (i + 1 < ac)
+				std::cout << " ";
+		}
 	std::cout << std::endl;
 }
